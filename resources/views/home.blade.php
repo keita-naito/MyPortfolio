9bb9b2d6236dc30057f47551b8f062f9f00f74d8
keita <atiek_nn_xx@yahoo.co.jp>
2019-10-09 18:39:45 +0900

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,180 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>portfolio</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css">
+    </head>
+    
+    <body>
+        
+        <header>
+            <div class="header-logo">keita-naito</div>
+            <div id="nav-drawer">
+                    <input id="nav-input" type="checkbox" class="nav-unshown">
+                    <label id="nav-open" for="nav-input"><span></span></label>
+                    <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+                    <div id="nav-content">
+                        <ul>
+                            <li><a href="#news">NEWS</a></li>
+                            <li><a href="#menu">MENU</a></li>
+                            <li><a href="#access">ACCESS</a></li>
+                            <li><a href="#reservation">ご予約</a></li>
+                            <li><a href="contact.html">お問い合わせ</a></li>
+                        </ul>
+                    </div>
+            </div>
+            <div class="header-list">
+                <ul>
+                    <li><a href="#news">NEWS</a></li>
+                    <li><a href="#menu">MENU</a></li>
+                    <li><a href="#access">ACCESS</a></li>
+                    <li><a href="#reservation">ご予約</a></li>
+                    <li><a href="contact.html">お問い合わせ</a></li>
+                </ul>
+            </div>
+            
+        </header>
+        
+        <main>
+            <img class="main-image" src="image/59cbc9be42454dd47ebe13c448683a1d.jpg">
+            <div class="news-wrapper">
+                <h2><a id="news">NEWS</a></h2>
+                <div class="news-contents">
+                    <div class="news-content">
+                        <img src="image/eaba6fdeaa2eafac64347b82a7b6d121.jpg">
+                        <h3>NEWS TITLE</h3>
+                        <p>2019.9.28</p>
+                        <hr>
+                        <p>記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、</p>
+                    </div>
+                    <div class="news-content">
+                        <img src="image/eaba6fdeaa2eafac64347b82a7b6d121.jpg">
+                        <h3>NEWS TITLE</h3>
+                        <p>2019.9.28</p>
+                        <hr>
+                        <p>記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、</p>
+                    </div>
+                    <div class="news-content">
+                        <img src="image/eaba6fdeaa2eafac64347b82a7b6d121.jpg">
+                        <h3>NEWS TITLE</h3>
+                        <p>2019.9.28</p>
+                        <hr>
+                        <p>記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、記事の内容、</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="reservation">
+                <img src="image/59cbc9be42454dd47ebe13c448683a1d.jpg">
+                <div class="reservation-section">
+                    <h2><a id="reservation">ご予約</a></h2>
+                    <h3>WEB予約</h3>
+                    <button type="button" onclick="location.href='#'">WEBで予約する</button>
+                    <h3>電話予約</h3>
+                    <a href="tel:03-1234-5678">
+                        <button type="button">03-1234-5678</button>
+                    </a>
+                    <p>
+                        ※当日のご予約はお電話でのみ承ります。
+                        <br>（受付時間：9:00 - 17:00）
+                        <br><br>
+                        ※施術中は電話対応が出来ない事がありますので、
+                        ご了承下さい。
+                        <br><br>
+                        ※ウェブ予約の際は確認メールをお送りいたします。
+                    </p>
+                </div>
+            </div>
+            
+            <div class="access-section">
+                <h2><a id="access">ACCESS</a></h2>
+             <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3246.7154816920597!2d139.26981931521186!3d35.53602808022844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13
+             .1!3m3!1m2!1s0x60190490dad26c33%3A0x715818a80d4096ed!2z44CSMjQzLTAzMDcg56We5aWI5bed55yM5oSb55Sy6YOh5oSb5bed55S65Y2K5Y6f77yS77yT77yZ77yV4oiS77yR!5e0!3m2!1sja!2sjp!4v
+             1569809892061!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                <div class="access-information">
+                    <div class="information-detail">
+                        <h4>【定休日】</h4>
+                        <p>　毎週火曜日</p>
+                        <h4>【営業時間】</h4>
+                        <p>　AM9:00～PM6:00</p>
+                        <hr>
+                        <p>※店舗横に駐車スペースが2台あります。</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="menu-section">
+                <h2><a id="menu">MENU</a></h2>
+                <table>
+                    <tr>
+                        <td>一般カット</td><td>￥4000円</td>
+                    </tr>
+                    <tr>
+                        <td>高校生カット</td><td>￥3500円</td>
+                    </tr>
+                    <tr>
+                        <td>カラーリング</td><td>￥3000円</td>
+                    </tr>
+                    <tr>
+                        <td>ストレートパーマ</td><td>￥8000円</td>
+                    </tr>
+                </table>
+                <h3>組み合わせや料金の詳細についてはこちらから</h3>
+                <button type="button" onclick="location.href='#'">WEBで予約する</button>
+            </div>
+            
+            <div class="stylist-section">
+                <div class="icon">
+                    <img src="image/IMG_6108.正方形.jpg">
+                </div>
+                <div class="side-text">
+                    <h2>STAFF</h2>
+                    <h4>内藤 啓太【keita naito】</h4>
+                    <p>コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、コメント、</p>
+                </div>
+            </div>
+        </main>
+        
+        <footer>
+            <div class="footer-area">
+                <div class="list-navbar">
+                    <h5 class="nav-title">お問い合わせ</h5>
+                    <ul>
+                        <li><a href="tel:03-1234-5678">03-1234-5678</a></li>
+                        <li><a href="mailto:sample_email@co.jp">sample_email@co.jp</a></li>
+                    </ul>
+                </div>
+                <div class="list-navbar">
+                    <h5 class="nav-title">CONTENTS</h5>
+                    <ul>
+                        <li>NEWS</li>
+                        <li>ご予約</li>
+                        <li>ACCESS</li>
+                        <li>MENU</li>
+                    </ul>
+                </div>
+                <div class="list-navbar outer">
+                    <h5 class="nav-title">ご予約</h5>
+                    <p>WEB予約</p>
+                    <button type="button" onclick="location.href='#'">WEBで予約する</button>
+                    <p>電話予約</p>
+                    <a href="tel:03-1234-5678">
+                    <button type="button">03-1234-5678</button>
+                    </a>
+                </div>
+                <div class="list-navbar">
+                    <h5 class="nav-title shop-information">店舗情報</h5>
+                    <p>・営業時間<br>【9:00～18:00】</p>
+                    <p>・定休日<br>毎週火曜日</p>
+                </div>
+            </div>
+            <div class="copy-right">
+                <p>©2019 ○○美容室 DESIGNED BY NAITO</p>
+            </div>
+        </footer>
+    </body>
+</html>
