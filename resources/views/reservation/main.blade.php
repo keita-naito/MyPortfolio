9c8a1103612e6dfb14585c9a49f8826c6b92988a
keita <atiek_nn_xx@yahoo.co.jp>
2019-10-28 16:47:51 +0900

    reservation correction

--- resources/views/reservation/main.blade.php
+++ resources/views/reservation/main.blade.php
@@ -6,8 +6,8 @@
     <div class="reservation-page">
         <h2>予約画面</h2>
         <div class="reservation-wrapper">
-            <iframe src="https://calendar.google.com/calendar/embed?src=etl9g7pdqok7ujkc4v3g35l8co%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="50%" height="600" frameborder="0" scrolling="no"></iframe>
-            <iframe class ="reservation-form" src="https://docs.google.com/forms/d/e/1FAIpQLScxyCeugVzDQPEIQABOUVIOG09-uVFBjZf50rqlaW7M9FB88A/viewform?embedded=true" width="50%" height="600" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+            <iframe class="reservation-calendar" src="https://calendar.google.com/calendar/embed?src=etl9g7pdqok7ujkc4v3g35l8co%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="50%" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe class="reservation-form" src="https://docs.google.com/forms/d/e/1FAIpQLScxyCeugVzDQPEIQABOUVIOG09-uVFBjZf50rqlaW7M9FB88A/viewform?embedded=true" width="50%" height="600" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
         </div>
     </div>
 @endsection
